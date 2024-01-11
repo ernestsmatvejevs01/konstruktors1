1281a3cad9fa77dc8d8c0033218640f00ab9a6ed
ks <ernestsmatvejevs01@gmail.com>
2024-01-11 21:19:49 +0000

    NASA API key ieraksts

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "dXvQFkmw5w8vxLbT0cGYEoLuoswdGyCnMPA43Gfv"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
