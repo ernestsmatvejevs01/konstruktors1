4f86643054117828db2c866a898b8b9a9488a1a5
ernestsmatvejevs01 <149001929+ernestsmatvejevs01@users.noreply.github.com>
2024-01-12 18:24:20 +0200

    added logging to project

--- naked.py
+++ naked.py
@@ -1,4 +1,6 @@
 # Importing necessary libraries/modules
+import logging
+import logging.config
 import requests # Lai veiktu HTTP pieprasījumus
 import json # Darbam ar JSON datiem
 import datetime # Datuma un laika apstrādei
@@ -7,6 +9,14 @@ import yaml # Darbam ar YAML datiem
 
 from datetime import datetime # 'datetime' klases importēšana no 'datetime' moduļa
 from configparser import ConfigParser
+# Loading logging configuration
+	with open('./log_worker.yaml', 'r') as stream:
+	    log_config = yaml.safe_load(stream)
+	logging.config.dictConfig(log_config)
+	# Creating logger
+	logger = logging.getLogger('root')
+
+
 # Ziņojums par programmas sākumu
 print('Asteroid processing service')
 
@@ -20,23 +30,22 @@ try:
 	except:
 		logger.exception('')
 	print('DONE')
-
 # NASA tuvāko Zemes objektu tīmekļa servisa API atslēga un URL
 
 # Šodienas datuma iegūšana
 dt = datetime.now()
 request_date = str(dt.year) + "-" + str(dt.month).zfill(2) + "-" + str(dt.day).zfill(2)  
-print("Generated today's date: " + str(request_date))
+logger.debug("Generated today's date: " + str(request_date))
 
 # API pieprasījuma URL izvadīšana
-print("Request url: " + str(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key))
+logger.debug("Request url: " + str(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key))
 # Pieprasījums NASA API
 r = requests.get(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key)
 
 # Informācijas izvadīšana par API atbildi
-print("Response status code: " + str(r.status_code))
-print("Response headers: " + str(r.headers))
-print("Response content: " + str(r.text))
+logger.debug("Response status code: " + str(r.status_code))
+logger.debug("Response headers: " + str(r.headers))
+logger.debug("Response content: " + str(r.text))
 
 # Pārbauda, vai API pieprasījums bija veiksmīgs (statusa kods 200)
 if r.status_code == 200:
@@ -49,7 +58,7 @@ if r.status_code == 200:
 	# Pārbauda, vai JSON datiem ir 'element_count'
 	if 'element_count' in json_data:
 		ast_count = int(json_data['element_count'])
-		print("Asteroid count today: " + str(ast_count))
+		logger.info("Asteroid count today: " + str(ast_count))
 		 # Pārbauda, vai šodien ir kādi asteroidi
 		if ast_count > 0:
 			# Iterācija cauri asteroidu sarakstam
@@ -97,7 +106,7 @@ if r.status_code == 200:
 							tmp_ast_close_appr_dt_utc = "1969-12-31 23:59:59"
 							tmp_ast_close_appr_dt = "1969-12-31 23:59:59"
 					else:
-						print("No close approach data in message")
+						logger.warning("No close approach data in message")
 						tmp_ast_close_appr_ts = 0
 						tmp_ast_close_appr_dt_utc = "1970-01-01 00:00:00"
 						tmp_ast_close_appr_dt = "1970-01-01 00:00:00"
@@ -105,10 +114,10 @@ if r.status_code == 200:
 						tmp_ast_miss_dist = -1
 					# Izvadīt asteroida informāciju
 
-					print("------------------------------------------------------- >>")
-					print("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
-					print("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
-					print("Speed: " + str(tmp_ast_speed) + " km/h" + " | MISS distance: " + str(tmp_ast_miss_dist) + " km")
+					logger.info("------------------------------------------------------- >>")
+					logger.info("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
+					logger.info("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
+					logger.info("Speed: " + str(tmp_ast_speed) + " km/h" + " | MISS distance: " + str(tmp_ast_miss_dist) + " km")
 					
 					# Pievienot asteroida datus atbilstošajam sarakstam
 					if tmp_ast_hazardous == True:
@@ -117,24 +126,24 @@ if r.status_code == 200:
 						ast_safe.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist])
 
 		else:
-			print("No asteroids are going to hit earth today")
+			logger.info("No asteroids are going to hit earth today")
 
-	print("Hazardous asteorids: " + str(len(ast_hazardous)) + " | Safe asteroids: " + str(len(ast_safe)))
+	logger.info("Hazardous asteorids: " + str(len(ast_hazardous)) + " | Safe asteroids: " + str(len(ast_safe)))
 	# Bīstamo asteroīdu kārtošana pēc tuvākā pieejas laika zīmes
 	if len(ast_hazardous) > 0:
 
 		ast_hazardous.sort(key = lambda x: x[4], reverse=False)
 
-		print("Today's possible apocalypse (asteroid impact on earth) times:")
+		logger.info("Today's possible apocalypse (asteroid impact on earth) times:")
 		 # Informācijas izvadīšana par bīstamiem asteroīdiem
 		for asteroid in ast_hazardous:
 			print(str(asteroid[6]) + " " + str(asteroid[0]) + " " + " | more info: " + str(asteroid[1]))
 
 		ast_hazardous.sort(key = lambda x: x[8], reverse=False)
 		 # Informācijas izvadīšana par bīstamo asteroīdu ar tuvāko pieeju attālumam
-		print("Closest passing distance is for: " + str(ast_hazardous[0][0]) + " at: " + str(int(ast_hazardous[0][8])) + " km | more info: " + str(ast_hazardous[0][1]))
+		logger.info("Closest passing distance is for: " + str(ast_hazardous[0][0]) + " at: " + str(int(ast_hazardous[0][8])) + " km | more info: " + str(ast_hazardous[0][1]))
 	else:
-		print("No asteroids close passing earth today")
+		logger.info("No asteroids close passing earth today")
 
 else:
-	print("Unable to get response from API. Response code: " + str(r.status_code) + " | content: " + str(r.text))
+	logger.error("Unable to get response from API. Response code: " + str(r.status_code) + " | content: " + str(r.text))
